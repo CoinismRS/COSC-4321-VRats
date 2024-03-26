e1f70a13221e3ff570574fa348332cbd214368cf
Michael <mtmichael500@gmail.com>
2024-03-25 23:42:20 -0500

    basic scene to change wall colors

---
+++ N-terior/Assets/Scripts/changeWallColor.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+using Unity.VisualScripting;
+
+public class changeWallColor : MonoBehaviour
+{
+    private OVRSceneManager oVRSceneManager;
+    private OVRSceneRoom room;
+    private OVRScenePlane[] walls;
+    public OVRScenePrefabOverride wallPrefab;
+    public OVRScenePrefabOverride windows;
+    public OVRScenePrefabOverride art;
+
+
+    private void Awake()
+    {
+        Debug.Log("Awakeeeeee");
+        oVRSceneManager = FindObjectOfType<OVRSceneManager>();
+        oVRSceneManager.SceneModelLoadedSuccessfully += SceneLoaded;
+        oVRSceneManager.PrefabOverrides.Add(wallPrefab);
+        oVRSceneManager.PrefabOverrides.Add(windows);
+        oVRSceneManager.PrefabOverrides.Add(art);
+        //Debug.Log("Overrides" + oVRSceneManager.PrefabOverrides.Count);
+        //oVRSceneManager.PrefabOverrides[0] = wallPrefab;
+
+
+    }
+    /*void Start()
+    {
+
+    }*/
+
+    void Update()
+    {
+
+        //oVRSceneManager.PrefabOverrides[0] = wallPrefab;
+
+
+    }
+
+
+
+
+    public void SceneLoaded()
+    {
+        Debug.Log("func started");
+        room = FindObjectOfType<OVRSceneRoom>();
+        if (room != null)
+        {
+            Debug.Log("Room Exsits");
+
+        }
+        walls = room.Walls;
+        Debug.Log("I am here dsfbsdjkfnkdjsfndsjk" + walls.Length);
+
+    }
+}
