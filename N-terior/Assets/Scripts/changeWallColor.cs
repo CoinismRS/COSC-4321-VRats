6aea2e9a5f683e1954bbb53badf1df17b483fd03
Michael <mtmichael500@gmail.com>
2024-03-26 17:37:10 -0500

    floor, wall, and ceiling colors can be switched in real time

--- N-terior/Assets/Scripts/changeWallColor.cs
+++ N-terior/Assets/Scripts/changeWallColor.cs
@@ -10,32 +10,52 @@ public class changeWallColor : MonoBehaviour
     private OVRSceneManager oVRSceneManager;
     private OVRSceneRoom room;
     private OVRScenePlane[] walls;
-    public OVRScenePrefabOverride wallPrefab;
-    public OVRScenePrefabOverride windows;
-    public OVRScenePrefabOverride art;
-
+    private OVRScenePlane ceiling;
+    private OVRScenePlane floor;
+    //public OVRScenePrefabOverride wallPrefab;
+    //public OVRScenePrefabOverride windows;
+    //public OVRScenePrefabOverride art;
+    public Material wallMat;
+    public Material floorMat;
+    //public Color wallColor;
 
     private void Awake()
     {
         Debug.Log("Awakeeeeee");
         oVRSceneManager = FindObjectOfType<OVRSceneManager>();
         oVRSceneManager.SceneModelLoadedSuccessfully += SceneLoaded;
-        oVRSceneManager.PrefabOverrides.Add(wallPrefab);
+        /*oVRSceneManager.PrefabOverrides.Add(wallPrefab);
         oVRSceneManager.PrefabOverrides.Add(windows);
-        oVRSceneManager.PrefabOverrides.Add(art);
+        oVRSceneManager.PrefabOverrides.Add(art);*/
         //Debug.Log("Overrides" + oVRSceneManager.PrefabOverrides.Count);
         //oVRSceneManager.PrefabOverrides[0] = wallPrefab;
 
 
     }
 
+
+    private void Update()
+    {
+        if (room != null)
+        {
+            var ceilingMaterial = ceiling.GetComponent<MeshRenderer>();
+            var floorMaterial = floor.GetComponent<MeshRenderer>();
+            ceilingMaterial.material = wallMat;
+            floorMaterial.material = floorMat;
+            foreach (var wall in walls)
+            {
+                var wallMaterial = wall.GetComponent<MeshRenderer>();
+                wallMaterial.material = wallMat;
+            }
+
+        }
+    }
+
     public void SceneLoaded()
     {
-        Debug.Log("func started");
         room = FindObjectOfType<OVRSceneRoom>();
         walls = room.Walls;
-        
-      
-
+        ceiling = room.Ceiling;
+        floor = room.Floor;
     }
 }
