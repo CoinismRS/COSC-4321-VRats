33bb58f28ebf43254cb08c454bb396bf05af67cf
Daniel Garza <danielrgarza12@gmail.com>
2024-04-10 08:33:39 -0500

    catalog: add json color info for loading script

---
+++ N-terior/Assets/Scripts/ColorInfo.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class ColorInfo
+{
+    public string name;
+    public string hex;
+}
+
+[System.Serializable]
+public class ColorList
+{
+    public List<ColorInfo> colors;
+}
