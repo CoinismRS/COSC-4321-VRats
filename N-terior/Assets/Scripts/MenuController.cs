fa432894d4a1b3b2b5a305f3a25d3e421f7f122a
Lacieness <96393215+Lacieness@users.noreply.github.com>
2024-04-12 19:42:34 -0500

    Display wall area on UI.

--- N-terior/Assets/Scripts/MenuController.cs
+++ N-terior/Assets/Scripts/MenuController.cs
@@ -2,9 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class MenuController : MonoBehaviour
 {
+  public FlexibleColorPicker fcp;
+  public TextMeshProUGUI paintName;
+  public TextMeshProUGUI wallArea;
+  public changeWallColor wallPainter;
+
   public void StartBtn()
   {
     SceneManager.LoadScene("MainScene");
@@ -13,5 +19,16 @@ public class MenuController : MonoBehaviour
   public void ExitBtn()
   {
     Application.Quit();
+  }
+
+  void Update()
+  {
+    Color currentColor = fcp.color;
+
+    string hexColor = ColorUtility.ToHtmlStringRGB(currentColor);
+    paintName.text = "#" + hexColor;
+
+    wallArea.text = "Area Size: " + wallPainter.paintAreaNeeded.ToString("F2") + " square units";
+
   }
 }
