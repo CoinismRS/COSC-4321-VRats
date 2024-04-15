33dda805e8e4febb2c2c8301c57b4f76067732b9
Dinuk <dinukkarunartne747@gmail.com>
2024-04-15 15:40:04 -0500

    Simple Prefab Spawner
    script to place objects using controler

---
+++ N-terior/Assets/Scripts/SimplePrefabSpawner.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class SimplePrefabSpawner : MonoBehaviour
+{
+    public GameObject prefab;
+    public GameObject previewPrefab;
+    private GameObject currentPreview;
+
+    private void Start() => currentPreview = Instantiate(previewPrefab);
+
+    private void Update()
+    {
+        Ray ray = new Ray(OVRInput.GetLocalControllerPosition(OVRInput.Controller.RTouch),
+                          OVRInput.GetLocalControllerRotation(OVRInput.Controller.RTouch) * Vector3.forward);
+
+        if (Physics.Raycast(ray, out RaycastHit hit))
+        {
+            currentPreview.transform.position = hit.point;
+            currentPreview.transform.rotation = Quaternion.FromToRotation(Vector3.up, hit.normal);
+
+            if (OVRInput.GetDown(OVRInput.Button.One))
+            {
+                Instantiate(prefab, hit.point, Quaternion.FromToRotation(Vector3.up, hit.normal));
+            }
+        }
+    }
+}
