181b109b07662d73f7cd868c5d824139f09e0239
j-uzr <116126823+j-uzr@users.noreply.github.com>
2024-04-16 15:35:26 -0500

    Refactored FloorChanger script

--- N-terior/Assets/Scripts/FloorChanger.cs
+++ N-terior/Assets/Scripts/FloorChanger.cs
@@ -10,32 +10,14 @@ public class floorChanger : MonoBehaviour
 {
     private OVRSceneManager oVRSceneManager;
     private OVRSceneRoom room;
-    private OVRScenePlane[] walls;
-    private OVRScenePlane ceiling;
     private OVRScenePlane floor;
-    //private PassthroughProjectionSurfaceBuildingBlock passThroughWindow;
-    //public OVRScenePrefabOverride wallPrefab;
-    //public OVRScenePrefabOverride windows;
-
-    public Material wallMat;
     public Material floorMat;
-    //public Color wallColor = Color.clear;
-    //public Color floorColor = Color.clear;
-    public FlexibleColorPicker fcp;
+
     private void Awake()
     {
-        //Debug.Log("Awakeeeeee");
         oVRSceneManager = FindObjectOfType<OVRSceneManager>();
         oVRSceneManager.SceneModelLoadedSuccessfully += SceneLoaded;
         fcp.color = Color.clear;
-        //Debug.Log("Color:" + fcp.color);
-        /*oVRSceneManager.PrefabOverrides.Add(wallPrefab);
-        oVRSceneManager.PrefabOverrides.Add(windows);
-        oVRSceneManager.PrefabOverrides.Add(art);*/
-        //Debug.Log("Overrides" + oVRSceneManager.PrefabOverrides.Count);
-        //oVRSceneManager.PrefabOverrides[0] = wallPrefab;
-
-
     }
 
 
@@ -43,36 +25,21 @@ public class floorChanger : MonoBehaviour
     {
         if (room != null)
         {
-            //var ceilingMaterial = ceiling.GetComponent<MeshRenderer>();
             var floorMaterial = floor.GetComponent<MeshRenderer>();
-            if (fcp.color != Color.clear)
+
+            if (floorMat != null)
             {
-                //wallMat.color = fcp.color;
-                //floorMat.color = floorColor;
-
-
-                //ceilingMaterial.enabled = true;
                 floorMaterial.enabled = true;
-                ceilingMaterial.material = wallMat;
-                floorMaterial.material = floorMat;
-                /*foreach (var wall in walls)
-                {
-                    var wallMaterial = wall.GetComponent<MeshRenderer>();
-                    wallMaterial.enabled = true;
-                    wallMaterial.material = wallMat;
-                }*/
             }
-        }
 
+            floorMaterial.material = floorMat;
+        }
     }
 
     public void SceneLoaded()
     {
         room = FindObjectOfType<OVRSceneRoom>();
-        //walls = room.Walls;
-        //ceiling = room.Ceiling;
         floor = room.Floor;
-
     }
 }
 
