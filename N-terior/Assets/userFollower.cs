ad25f1aa676995f7a4059ea35f8914f8344364a9
Michael <mtmichael500@gmail.com>
2024-04-21 18:33:24 -0500

    cleaned up code

--- N-terior/Assets/userFollower.cs
+++ N-terior/Assets/userFollower.cs
@@ -4,54 +4,8 @@ using UnityEngine;
 
 public class userFollower : MonoBehaviour
 {
-    // Start is called before the first frame update
-    /* [SerializeField] private Transform cameraTransform;
-     [SerializeField] private float distance = 3.0f;
-
-     private bool isCentered = false;
-
-     // Built-in Unity *Magic*
-     private void OnBecameInvisible()
-     {
-         isCentered = false;
-     }
-
-     private void Update()
-     {
-         if (!isCentered)
-         {
-             // Find the position we need to be at
-             Vector3 targetPosition = FindTargetPosition();
-
-             // Move just a little bit at a time
-             MoveTowards(targetPosition);
-
-             // If we've reached the position, don't do anymore work
-             if (ReachedPosition(targetPosition))
-                 isCentered = true;
-         }
-     }
-
-     private Vector3 FindTargetPosition()
-     {
-         // Let's get a position infront of the player's camera
-         return cameraTransform.position + (cameraTransform.forward * distance);
-     }
-
-     private void MoveTowards(Vector3 targetPosition)
-     {
-         // Instead of a tween, that would need to be constantly restarted
-         transform.position += (targetPosition - transform.position) * 0.025f;
-     }
-
-     private bool ReachedPosition(Vector3 targetPosition)
-     {
-         // Simple distance check, can be replaced if you wish
-         return Vector3.Distance(targetPosition, transform.position) < 0.1f;
-     }*/
 
     [SerializeField] private Transform lookAt;
-    [SerializeField] private Transform transformToFollow;
     [SerializeField] private float followSpeed;
     private Transform _thisTransform;
 
@@ -59,9 +13,7 @@ public class userFollower : MonoBehaviour
     [SerializeField] private float distance = 10.0f;
 
     private bool isCentered = false;
-    public MeshRenderer mr;
 
-    // Built-in Unity *Magic*
     private void OnBecameInvisible()
     {
         isCentered = false;
@@ -86,35 +38,30 @@ public class userFollower : MonoBehaviour
 
         if (!isCentered)
         {
-            Debug.Log("I have running");
-            // Find the position we need to be at
+            
             Vector3 targetPosition = FindTargetPosition();
 
-            // Move just a little bit at a time
             MoveTowards(targetPosition);
 
-            // If we've reached the position, don't do anymore work
             if (ReachedPosition(targetPosition))
                 isCentered = true;
-                Debug.Log("I have stopped");
+               
         }
     }
 
     private Vector3 FindTargetPosition()
     {
-        // Let's get a position infront of the player's camera
         return cameraTransform.position + (cameraTransform.forward * distance);
     }
 
     private void MoveTowards(Vector3 targetPosition)
     {
-        // Instead of a tween, that would need to be constantly restarted
         transform.position += (targetPosition - transform.position) * 0.025f;
     }
 
     private bool ReachedPosition(Vector3 targetPosition)
     {
-        // Simple distance check, can be replaced if you wish
+        
         return Vector3.Distance(targetPosition, transform.position) < 0.1f;
     }
 }
